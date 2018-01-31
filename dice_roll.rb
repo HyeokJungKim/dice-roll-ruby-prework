aafb9d6b9c1bdef159fffac3abd0a19bd84b3d56
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-01-31 19:02:52 +0000

    automatically backed up by learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,12 +2,12 @@
 
 # Feel free to google "how to generate a random number in ruby"
 #
-# def roll
-#   # code goes here
-#   return rand(1..6)
-# end
-
 def roll
+  # code goes here
+  return rand(1..6)
+end
+
+def roll2
   arr = [1,2,3,4,5,6]
   return arr[rand(6)]
 
